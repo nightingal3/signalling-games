2adff27c8477e8169420b0a7e1c0cdf4812b4421
Emmy Liu <emmy.liu.1998@gmail.com>
2021-04-12 15:12:17 -0400

    3x2 update

--- price_of_anarchy.py
+++ price_of_anarchy.py
@@ -1,7 +1,9 @@
 import csv
+import numpy as np
 import pickle
 from fractions import Fraction
 import pdb
+from typing import List
 from pprint import pprint
 
 def fix_spacing(filename: str) -> None:
@@ -28,6 +30,7 @@ def read_nash_file(filename: str, key_filename: str, pure_only: bool = True) -> 
         for line in reader:
             if len(line) == 0 or line[0] == "*lrsnash:lrslib":
                 continue
+            line = [i for i in line if i != ""]
             if line[0] == "2":
                 strategy_vec = [float(Fraction(s)) for s in line[1:-1]]
                 if pure_only:
@@ -57,8 +60,41 @@ def read_nash_file(filename: str, key_filename: str, pure_only: bool = True) -> 
     if pure_only:
         return pure_strategies
 
-def price_of_anarchy(pure_strategies: List, n: int, m: int) -> float:
-    max_payoff = min([n, m])
+def price_of_anarchy(pure_strategies: List, n: int, m: int, probs: List, costs: List) -> float:
+    # do general case later...
+    if n == 2 and m == 2: 
+        base_sender_matrix = np.identity(2)
+        base_sender_matrix_alt = np.flip(base_sender_matrix, axis=1)
+        base_receiver_matrix = np.identity(2)
+        base_receiver_matrix_alt = np.flip(base_receiver_matrix, axis=1)
+        sender_matrix = (base_sender_matrix.T * probs).T
+        receiver_matrix = base_receiver_matrix/costs
+        alt_sender_matrix = (base_sender_matrix_alt.T * probs).T
+        alt_receiver_matrix = (base_receiver_matrix_alt.T * probs).T
+        max_payoff = max(np.trace(np.matmul(sender_matrix, receiver_matrix)), np.trace(np.matmul(alt_sender_matrix, alt_receiver_matrix)))
+
+    elif n == 3 and m == 2:
+        signals_by_cost = sorted(costs)
+        highest_probs = sorted(probs, reverse=True)
+    
+        best_events = highest_probs[:2]
+        best_indices_sender = np.where(np.in1d(probs, best_events))[0]
+        cheapest_signal, other = list(costs).index(signals_by_cost[0]), list(costs).index(signals_by_cost[1])
+        best_indices_receiver = [cheapest_signal, other]
+        nonzero_indices_s = (tuple(best_indices_sender), tuple(best_indices_receiver))
+        nonzero_indices_r = (tuple(best_indices_receiver), tuple(best_indices_sender))
+
+        base_sender_matrix = np.zeros((n, m))
+        base_sender_matrix[nonzero_indices_s] = 1
+
+        base_receiver_matrix = np.zeros((m, n))
+        base_receiver_matrix[nonzero_indices_r] = 1
+
+        sender_matrix = (base_sender_matrix.T * probs).T
+        receiver_matrix = ((base_receiver_matrix.T)/costs).T
+
+        max_payoff = np.trace(np.matmul(sender_matrix, receiver_matrix))
+
     worst_payoff = float("inf")
     worst_sender = None
     worst_receiver = None
@@ -69,10 +105,43 @@ def price_of_anarchy(pure_strategies: List, n: int, m: int) -> float:
             worst_receiver = strategy["receiver_matrix"] 
             worst_payoff = strategy["payoff"]
 
-    return worst_payoff
+    return worst_payoff/max_payoff
 
-def price_of_stability(pure_strategies: List, n: int, m: int) -> float:
-    max_payoff = min([n, m])
+def price_of_stability(pure_strategies: List, n: int, m: int, probs: List, costs: List) -> float:
+    if n == 2 and m == 2:
+        base_sender_matrix = np.identity(2)
+        base_sender_matrix_alt = np.flip(base_sender_matrix, axis=1)
+        base_receiver_matrix = np.identity(2)
+        base_receiver_matrix_alt = np.flip(base_receiver_matrix, axis=1)
+        sender_matrix = (base_sender_matrix.T * probs).T
+        receiver_matrix = base_receiver_matrix/costs
+        alt_sender_matrix = (base_sender_matrix_alt.T * probs).T
+        alt_receiver_matrix = (base_receiver_matrix_alt.T * probs).T
+        max_payoff = max(np.trace(np.matmul(sender_matrix, receiver_matrix)), np.trace(np.matmul(alt_sender_matrix, alt_receiver_matrix)))
+    
+    if n == 3 and m == 2:
+        signals_by_cost = sorted(costs)
+        highest_probs = sorted(probs, reverse=True)
+    
+        best_events = highest_probs[:2]
+        best_indices_sender = np.where(np.in1d(probs, best_events))[0]
+        cheapest_signal, other = list(costs).index(signals_by_cost[0]), list(costs).index(signals_by_cost[1])
+        best_indices_receiver = [cheapest_signal, other]
+        nonzero_indices_s = (tuple(best_indices_sender), tuple(best_indices_receiver))
+        nonzero_indices_r = (tuple(best_indices_receiver), tuple(best_indices_sender))
+
+        base_sender_matrix = np.zeros((n, m))
+        base_sender_matrix[nonzero_indices_s] = 1
+
+        base_receiver_matrix = np.zeros((m, n))
+        base_receiver_matrix[nonzero_indices_r] = 1
+
+        sender_matrix = (base_sender_matrix.T * probs).T
+        receiver_matrix = ((base_receiver_matrix.T)/costs).T
+
+        max_payoff = np.trace(np.matmul(sender_matrix, receiver_matrix))
+
+
     best_payoff = -float("inf")
     best_sender = None
     best_receiver = None
@@ -83,7 +152,7 @@ def price_of_stability(pure_strategies: List, n: int, m: int) -> float:
             best_receiver = strategy["receiver_matrix"] 
             best_payoff = strategy["payoff"]
 
-    return best_payoff
+    return best_payoff/max_payoff
 
 
 
--- run_simulations.py
+++ run_simulations.py
@@ -1,11 +1,19 @@
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import entropy
 from typing import List
 import pdb
 import subprocess
+import time
 
 from price_of_anarchy import *
 from enumerate_rewards import main
+from timeout import *
+
+TRIALS_DIR = "nash_trials"
+FIGURES_DIR = "nash_figures"
 
 def gen_random_probs(n: int) -> List:
     unnormalized = np.random.randint(low=1, high=100, size=n)
@@ -19,36 +27,149 @@ def random_simulation(n: int, m: int, granularity: float, out_filename: str, key
     y_coords = []
     prices_of_anarchy = []
     prices_of_stability = []
+    all_probs = []
+    all_costs = []
 
+    # randomly generated points
     for sim_num in range(num_simulations):
+        start = time.time()
         probs = gen_random_probs(n)
         costs = gen_random_costs(m)
 
         entropy_probs = entropy(probs)
         entropy_costs = entropy(costs/sum(costs))
         
-        # create the input files to send to lrsnash
-        main(n, m, granularity, probs, costs, out_filename, key_filename)
+        out_fname = f"{out_filename[:-4]}_{sim_num}"
+        key_fname = f"{key_filename[:-2]}_{sim_num}.p"
+        print("probs: ", probs)
+        print("costs: ", costs)
 
-        # solve for nash equilibria - ensure lrsnash is installed 
-        solve_ne = subprocess.run(["lrsnash", out_filename, ">", f"{out_filename[:-4]}_sol.txt"], check=True)
-
-        # get PoA/PoS
-        pure_strategies = read_nash_file(f"{out_filename[:-4]}_sol.txt", key_filename, pure_only=True)
-        PoA = price_of_anarchy(pure_strategies, n, m)
-        PoS = price_of_stability(pure_strategies, n, m)
+        try:
+            PoA, PoS = sim_with_params(n, m, granularity, probs, costs, out_fname, key_fname)
+        except:
+            num_simulations += 1
+            continue
 
         x_coords.append(entropy_probs)
         y_coords.append(entropy_costs)
         prices_of_anarchy.append(PoA)
         prices_of_stability.append(PoS)
+        all_probs.append(probs)
+        all_costs.append(costs)
+        end = time.time()
+        print("time: ", end - start)
+        
+        print(PoA)
+        print(PoS)
 
-    return x_coords, y_coords, prices_of_anarchy, prices_of_stability
+    # add a few anchor points for the extremes
+    simulations = [
+        {
+            "probs": [1/n  for _ in range(n)],
+            "costs": [1 for _ in range(m)],
+            "name": "trial_ext_1"
+        },
+        {
+            "probs": [0.99] + [0.01/(n - 1) for _ in range(n - 1)],
+            "costs": [1 for _ in range(m)],
+            "name": "trial_ext_2"
+        },
+        {
+            "probs": [1/n for _ in range(n)],
+            "costs": [10 ** (i + 1) for i in range(m)],
+            "name": "trial_ext_3"
+        },
+        {
+            "probs": [0.99] + [0.01/(n - 1) for _ in range(n - 1)],
+            "costs": [10 ** (i + 1) for i in range(m)],
+            "name": "trial_ext_4"
+        },
+        {
+            "probs": [1] + [0] * (n - 1),
+            "costs": [1 for _ in range(m)],
+            "name": "trial_ext_5"
+        },
+        {
+            "probs": [1] + [0] * (n - 1),
+            "costs": [10 ** (i + 1) for i in range(m)],
+            "name": "trial_ext_6"
+        }
+    ]
+
+    for sim in simulations:
+        name = sim["name"]
+        try:
+            PoA, PoS = sim_with_params(n, m, granularity, sim["probs"], sim["costs"], f"{out_filename[:-4]}_{name}", f"{key_filename[:-2]}_{name}")
+        except:
+            continue
+        x_coords.append(entropy(sim["probs"]))
+        y_coords.append(entropy(sim["costs"]))
+        prices_of_anarchy.append(PoA)
+        prices_of_stability.append(PoS)
+        all_probs.append(sim["probs"])
+        all_costs.append(sim["costs"])
+
+    return x_coords, y_coords, prices_of_anarchy, prices_of_stability, all_probs, all_costs
+
+@timeout(360)
+def sim_with_params(n: int, m: int, granularity: float, probs: List, costs: List, out_fname: str, key_fname: str) -> tuple:
+    # create the input files to send to lrsnash
+    main(n, m, granularity, probs, costs, f"{TRIALS_DIR}/{out_fname}", f"{TRIALS_DIR}/{key_fname}")
+
+    # solve for nash equilibria - ensure lrsnash is installed 
+    try:
+        with open(f"{TRIALS_DIR}/{out_fname}_sol.txt", "w") as sol_f:
+            solve_ne = subprocess.run(["lrsnash", f"{TRIALS_DIR}/{out_fname}.txt"], check=True, stdout=sol_f)
+    except:
+        raise TimeoutError
+
+    # get PoA/PoS
+    pure_strategies = read_nash_file(f"{TRIALS_DIR}/{out_fname}_sol.txt", f"{TRIALS_DIR}/{key_fname}", pure_only=True)
+    PoA = price_of_anarchy(pure_strategies, n, m, probs, costs)
+    PoS = price_of_stability(pure_strategies, n, m, probs, costs)
+
+    return PoA, PoS
+
+def graph_entropy_and_ne(x_coords: List, y_coords: List, z_coords: List, title: str = "Price of Anarchy", out_filename: str = "test.png") -> None:
+    norm = SqueezedNorm(vmin=0, vmax=max(z_coords), mid=1)
+    plt.tricontourf(x_coords, y_coords, z_coords, cmap="viridis", norm=norm, levels=[0, 0.2, 0.4, 0.6, 0.8, 1, 2, 4, 8, 16])
+    plt.xlabel("Entropy of probability distribution")
+    plt.ylabel("Entropy of signal costs")
+    plt.colorbar()
+    plt.title(title)
+
+    plt.savefig(f"{FIGURES_DIR}/{out_filename}")
 
 
+# credit to ImportanceOfBeingEarnest for this graphing class: 
+# https://stackoverflow.com/questions/44432693/change-colorbar-gradient-in-matplotlib
+class SqueezedNorm(matplotlib.colors.Normalize):
+    def __init__(self, vmin=None, vmax=None, mid=0, s1=2, s2=2, clip=False):
+        self.vmin = vmin # minimum value
+        self.mid  = mid  # middle value
+        self.vmax = vmax # maximum value
+        self.s1=s1; self.s2=s2
+        f = lambda x, zero,vmax,s: np.abs((x-zero)/(vmax-zero))**(1./s)*0.5
+        self.g = lambda x, zero,vmin,vmax, s1,s2: f(x,zero,vmax,s1)*(x>=zero) - \
+                                             f(x,zero,vmin,s2)*(x<zero)+0.5
+        matplotlib.colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        r = self.g(value, self.mid,self.vmin,self.vmax, self.s1,self.s2)
+        return np.ma.masked_array(r)
 
 
 if __name__ == "__main__":
-    random_simulation(3, 2, "test.txt")
+    N = 3
+    M = 2
+    TRIALS_DIR = f"{TRIALS_DIR}/{N}_{M}"
+    x, y, anarchy, stability, all_probs, all_costs = random_simulation(N, M, 0.25, "test_3_2.txt", "test_3_2_key.p", num_simulations=10)
+    graph_entropy_and_ne(x, y, anarchy, out_filename="3_2_anarchy.png")
+    plt.gcf().clear()
+    graph_entropy_and_ne(x, y, stability, title="Price of stability", out_filename="3_2_stability.png")
+
+    with open(f"{TRIALS_DIR}/pickled/simulation.p", "rb") as p_file:
+        simulation_data = {"entropy_probs": x, "entropy_costs": y, "probs": all_probs, "costs": all_costs,  "price_anarchy": anarchy, "price_stability": stability}
+
 
     
